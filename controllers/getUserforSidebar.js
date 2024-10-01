0b0b2ad8ed8588b05bffd18900d16dd9b39a094a
sujeet kumar sharma <sujeetsharma80811@gmail.com>
2024-10-01 23:09:34 +0530

    hi

--- controllers/getUserforSidebar.js
+++ controllers/getUserforSidebar.js
@@ -2,11 +2,15 @@ import User from "../models/userSchema.js";
 
 export const getUserForSidebar = async (req, res) => {
   try {
+
+    console.log("req.existingUser._id", req.existingUser._id)
     const loggedUserId = req.existingUser._id;
 
-    const filteredUser = await User.find({ _id: { $ne: loggedUserId } }).select(
+    const filteredUser = await User.find({ _id : { $ne: loggedUserId } }).select(
       "-password"
     );
+
+    console.log("{ _id : { $ne: loggedUserId }", { _id : { $ne: loggedUserId }})
 
     res.status(200).json({
       success: true,
